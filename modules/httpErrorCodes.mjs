2927c14786b52f4ccbba615867a71340441db3b4
frode lund <156180539+frlund@users.noreply.github.com>
2024-01-23 14:33:52 +0100

    startet med litt kode

--- modules/httpErrorCodes.mjs
+++ modules/httpErrorCodes.mjs
@@ -5,17 +5,26 @@
 class HttpCodes {
 
     static SuccesfullRespons = {
-        Ok: 200
+        Ok: 200 // Suksess, alt bare bra
     }
 
     static ClientSideErrorRespons = {
-        BadRequest: 400,
-        Unauthorized: 401,
-        PaymentRequired: 402,
-        Forbidden: 404,
-        NotFound: 404,
-        MethodNotAllowed: 405,
-        NotAcceptable: 406
+        BadRequest: 400,        // Feil ved anmoding/forespørsel
+        Unauthorized: 401,      // Uautorisert
+        PaymentRequired: 402,   // Betaling påkrevd
+        Forbidden: 403,         // Ikke tilstrekkelige tillatelser
+        NotFound: 404,          // Ressurs ikke funnet
+        MethodNotAllowed: 405,  // Metode er ikke tillatt
+        NotAcceptable: 406      // Uakseptabelt
+    }
+
+    static ServerSideErrorResponse = {
+        InternalServerError: 500,   // SERVER - Intern tjenerfeil
+        NotImplemented: 501,        // SERVER - Ikke implementert/støttet
+        BadGateway: 502,            // SERVER - Feil på proxy
+        ServiceUnavailable: 503,    // SERVER - Utilgjengelig tjeneste
+        GatewayTimeout: 504,        // SERVER - Tidsavbrudd proxy
+        HTTPVersionNotSupported: 505// SERVER - Ukjent HTTP-versjon, støttes ikke
     }
 
 }
--- modules/user.mjs
+++ modules/user.mjs
@@ -1,11 +1,15 @@
+
+// BRUKERINFORMASJON
 class User {
 
     constructor() {
         ///TODO: Are these the correct fields for your project?
-        this.email;
-        this.pswHash;
-        this.name;
+        this.email = '';
+        this.pswHash = '';
+        this.name = '';
     }
+
+
 }
 
 export default User;
