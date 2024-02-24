451897c15deb2ea681b38ef970a7aca1cd829d94
frode lund <156180539+frlund@users.noreply.github.com>
2024-02-24 16:44:53 +0100

    Oppdatert logging av hendelser

--- modules/SuperLogger.mjs
+++ modules/SuperLogger.mjs
@@ -33,17 +33,10 @@ class SuperLogger {
         VERBOSE: 5,     // We output a lott, but not 
         NORMAL: 10,     // We output a moderate amount of messages
         IMPORTANT: 100, // We output just siginfican messages
-        CRTICAL: 999    // We output only errors. 
+        CRITICAL: 999   // We output only errors. 
     };
 
-    // What level of messages should we be logging.
-    // This field is static 
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
     #globalThreshold = SuperLogger.LOGGING_LEVELS.NORMAL;
-
-    // Structure to keep the misc log functions that get created.
-    // This field is private.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_properties
     #loggers;
 
     //#region Using a variation on the singelton pattern
@@ -64,12 +57,10 @@ class SuperLogger {
     //#endregion
 
     static log(msg, logLevl = SuperLogger.LOGGING_LEVELS.NORMAL) {
-
         let logger = new SuperLogger();
         if (logger.#globalThreshold > logLevl) {
             return;
         }
-
         logger.#writeToLog(msg);
     }
 
@@ -81,49 +72,39 @@ class SuperLogger {
     }
 
     createLimitedHTTPRequestLogger(options) {
-
-        // if no level is threshold, we default to NORMAL.
-        // We are using the logical or (||) 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
         const threshold = options.threshold || SuperLogger.LOGGING_LEVELS.NORMAL;
-
-        // Returning an anonymous function that binds local scope.
         return (req, res, next) => {
-
-            // If the threshold provided is less then the global threshold, we do not logg
             if (this.#globalThreshold > threshold) {
                 return;
-            }
-
-            // Finaly we parse our request on to the method that is going to writ the log msg.
+            }          
             this.#LogHTTPRequest(req, res, next);
         }
 
     }
 
     #LogHTTPRequest(req, res, next) {
-        // These are just some variables that we extract to show the point 
-        // TODO: Extract and format information important for your dev process. 
         let type = req.method;
         const path = req.originalUrl;
         const when = new Date().toLocaleTimeString();
-
-        // TODO: This is just one simple thing to create structure and order. Can you do more?
         type = colorize(type);
-        this.#writeToLog([when, type, path].join(" "));
-
-        // On to the next handler function
+        const logMessage = `${when} | ${type} | ${path} | IP: ${req.ip} | Status: ${res.statusCode}`;
+        this.#writeToLog(logMessage);
         next();
     }
 
-    #writeToLog(msg) {
-
+   async #writeToLog(msg) {
         msg += "\n";
         console.log(msg);
-        ///TODO: The files should be based on current date.
-        // ex: 300124.log
-        fs.appendFile("./log.txt", msg, { encoding: "utf8" }, (err) => { });
-    }
+        const currentDate = new Date().toISOString().split('T')[0];
+        const logDir = './logs';
+        const logFileName = `${logDir}/log_${currentDate}.txt`;
+       
+        try {
+            await fs.appendFile(logFileName, msg, { encoding: "utf8" });
+        } catch (error) {
+            console.error("Error writing to log file:", error);
+        }
+   }
 }
 
 
