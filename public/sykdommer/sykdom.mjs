db46459a7f44957e2f01db84e9c32358f3c38fbb
frode lund <156180539+frlund@users.noreply.github.com>
2024-03-04 14:47:40 +0100

    Oppdatere layout sykdommer

---
+++ public/sykdommer/sykdom.mjs
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const closeButton = document.getElementById('closeButton');
+
+    closeButton.addEventListener('click', function() {
+        window.location.href = '../meny.html';
+    });
+
+});
