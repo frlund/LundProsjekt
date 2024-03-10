a332dbbf049d77715135267f0c438bf3c00b09be
frode lund <156180539+frlund@users.noreply.github.com>
2024-03-10 12:08:45 +0100

    Designoppdatering sykdommer

--- public/sykdommer/sykdom.mjs
+++ public/sykdommer/sykdom.mjs
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const closeButton = document.getElementById('closeButton');
+    const image = document.getElementById('mainImage');
 
     closeButton.addEventListener('click', function() {
         window.location.href = '../meny.html';
     });
 
+    image.addEventListener('click', function() {
+        if (image.style.width === '300px') {
+            image.style.width = '100%';
+        } else {
+            image.style.width = '300px';
+        }
+    });
 });
